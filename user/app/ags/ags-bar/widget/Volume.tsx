eeea4712e4208f0eb6be0f2d9303e8d33926a4b0
apexu <jj.zelger@proton.me>
2025-09-16 02:51:51 +0200

    finish volume widget

--- user/app/ags/ags-bar/widget/Volume.tsx
+++ user/app/ags/ags-bar/widget/Volume.tsx
@@ -1,7 +1,7 @@
 import { Gdk, Gtk } from "ags/gtk4";
 import { subprocess } from "ags/process";
 import Wp from "gi://AstalWp?version=0.1";
-import { createState, With } from "gnim";
+import { createBinding, createState, With } from "gnim";
 
 const VOLUME_SCROLL_STEP = 0.01;
 
@@ -119,187 +119,204 @@ export default function Volume() {
   }
 
   return (
-    <box>
+    <box hexpand={false}>
       <With value={state}>
         {(s) => (
-          <box hexpand={false}>
-            <button
-              class="module"
-              $={(self) => {
-                const controller = new Gtk.EventControllerScroll();
-                controller.set_flags(Gtk.EventControllerScrollFlags.VERTICAL);
+          <button
+            class="module"
+            css={"min-width: 72px;"}
+            $={(self) => {
+              const controller = new Gtk.EventControllerScroll();
+              controller.set_flags(Gtk.EventControllerScrollFlags.VERTICAL);
 
-                controller.connect("scroll", (_c, _dx, dy) => {
-                  if (dy < 0) {
-                    adjustVolume("up");
-                  } else {
-                    adjustVolume("down");
-                  }
+              controller.connect("scroll", (_c, _dx, dy) => {
+                if (dy < 0) {
+                  adjustVolume("up");
+                } else {
+                  adjustVolume("down");
+                }
 
-                  return true;
-                });
-                self.add_controller(controller);
+                return true;
+              });
+              self.add_controller(controller);
 
-                self.connect("clicked", () => {
-                  if (popover) {
-                    popover.set_pointing_to(self.get_allocation());
-                    popover.popup();
-                  }
-                });
-              }}
-            >
-              <box halign={Gtk.Align.CENTER}>
-                <image
-                  iconName={getVolumeIcon(s.outputVolume, s.outputIsMuted)}
-                  iconSize={Gtk.IconSize.NORMAL}
-                  css="margin-right: 8px;"
-                />
-                <label label={s.outputVolume.toString() + "%"} />
-              </box>
-            </button>
-
-            <popover
-              $={(p) => {
-                popover = p;
-                p.set_has_arrow(false);
-                p.set_cascade_popdown(false);
-                p.set_autohide(true);
-              }}
-              widthRequest={450}
-              class="volume-popover"
-            >
-              <box orientation={Gtk.Orientation.VERTICAL} spacing={8}>
-                <centerbox
-                  orientation={Gtk.Orientation.HORIZONTAL}
-                  css={"padding-left: 8px;"}
-                >
-                  <label
-                    label={"Audio"}
-                    $type="start"
-                    css={"font-size: 20px; font-weight: bold;"}
-                  />
-                  <button
-                    class="volume-settings-button"
-                    $type="end"
-                    onClicked={handleSettingsButtonClicked}
-                  >
-                    <image
-                      iconName={"preferences-desktop-sound"}
-                      iconSize={Gtk.IconSize.NORMAL}
-                    />
-                  </button>
-                </centerbox>
-                <box orientation={Gtk.Orientation.HORIZONTAL} spacing={8}>
-                  <image
-                    iconName="audio-headphones"
-                    iconSize={Gtk.IconSize.LARGE}
-                  />
-                  <Gtk.DropDown
-                    hexpand={true}
-                    class="dropdown"
-                    overflow={Gtk.Overflow.HIDDEN}
-                    $={(self) => {
-                      const outputs = s.allDevices.filter(
-                        (s) => s.outputRoutes.length !== 0,
-                      );
-
-                      const store = new Gtk.StringList({
-                        strings: outputs.map((d) => d.description),
-                      });
-                      self.set_model(store);
-
-                      if (s.currentOutput)
-                        self.set_selected(
-                          outputs.findIndex(
-                            (d) => d.id === s.currentOutput?.device_id,
-                          ),
-                        );
-
-                      self.connect("notify::selected", () => {
-                        const idx = self.get_selected();
-                        const output = outputs[idx];
-
-                        const speaker = wp.audio.speakers.find(
-                          (s) => s.device_id === output.id,
-                        );
-
-                        if (speaker) {
-                          speaker.set_is_default(true);
-                          setState({
-                            ...state.get(),
-                            currentOutput: speaker,
-                          });
-                        }
-                      });
-                    }}
-                  />
-                </box>
-
-                <box orientation={Gtk.Orientation.HORIZONTAL} spacing={8}>
-                  <image
-                    iconName="audio-input-microphone"
-                    iconSize={Gtk.IconSize.LARGE}
-                  />
-                  <Gtk.DropDown
-                    hexpand={true}
-                    class="dropdown"
-                    $={(self) => {
-                      const inputs = s.allDevices.filter(
-                        (s) => s.inputRoutes.length !== 0,
-                      );
-
-                      const store = new Gtk.StringList({
-                        strings: inputs.map((d) => d.description),
-                      });
-                      self.set_model(store);
-
-                      if (s.currentOutput)
-                        self.set_selected(
-                          inputs.findIndex(
-                            (d) => d.id === s.currentInput?.device_id,
-                          ),
-                        );
-
-                      self.connect("notify::selected", () => {
-                        const idx = self.get_selected();
-                        const input = inputs[idx];
-
-                        const microphone = wp.audio.microphones.find(
-                          (s) => s.device_id === input.id,
-                        );
-
-                        if (microphone) {
-                          microphone.set_is_default(true);
-                          setState({
-                            ...state.get(),
-                            currentInput: microphone,
-                          });
-                        }
-                      });
-                    }}
-                  />
-                </box>
-                <Gtk.Separator />
-                <box orientation={Gtk.Orientation.HORIZONTAL}>
-                  <image
-                    iconName="audio-headphones"
-                    iconSize={Gtk.IconSize.NORMAL}
-                  />
-                  <slider
-                    min={0}
-                    max={1}
-                    hexpand={true}
-                    value={s.currentOutput?.volume!}
-                    onChangeValue={({ value }) =>
-                      s.currentOutput?.set_volume(value)
-                    }
-                  />
-                </box>
-              </box>
-            </popover>
-          </box>
+              self.connect("clicked", () => {
+                if (popover) {
+                  popover.set_pointing_to(self.get_allocation());
+                  popover.popup();
+                }
+              });
+            }}
+          >
+            <box halign={Gtk.Align.CENTER}>
+              <image
+                iconName={getVolumeIcon(s.outputVolume, s.outputIsMuted)}
+                iconSize={Gtk.IconSize.NORMAL}
+                css="margin-right: 8px;"
+              />
+              <label label={s.outputVolume.toString() + "%"} />
+            </box>
+          </button>
         )}
       </With>
+
+      <popover
+        $={(p) => {
+          popover = p;
+        }}
+        widthRequest={450}
+      >
+        <box
+          orientation={Gtk.Orientation.VERTICAL}
+          spacing={8}
+          class="volume-popover"
+        >
+          <centerbox orientation={Gtk.Orientation.HORIZONTAL}>
+            <label
+              label={"Audio"}
+              $type="start"
+              css={"font-size: 20px; font-weight: bold;"}
+            />
+            <button
+              class="volume-settings-button"
+              $type="end"
+              onClicked={handleSettingsButtonClicked}
+            >
+              <image
+                iconName={"preferences-desktop-sound"}
+                iconSize={Gtk.IconSize.NORMAL}
+              />
+            </button>
+          </centerbox>
+
+          <box>
+            <With value={state}>
+              {(s) => (
+                <box orientation={Gtk.Orientation.VERTICAL} spacing={8}>
+                  <box orientation={Gtk.Orientation.HORIZONTAL} spacing={8}>
+                    <image
+                      iconName="audio-headphones"
+                      iconSize={Gtk.IconSize.NORMAL}
+                    />
+                    <Gtk.DropDown
+                      hexpand={true}
+                      class="dropdown"
+                      overflow={Gtk.Overflow.HIDDEN}
+                      $={(self) => {
+                        const outputs = s.allDevices.filter(
+                          (s) => s.outputRoutes.length !== 0,
+                        );
+
+                        const store = new Gtk.StringList({
+                          strings: outputs.map((d) => d.description),
+                        });
+                        self.set_model(store);
+
+                        if (s.currentOutput)
+                          self.set_selected(
+                            outputs.findIndex(
+                              (d) => d.id === s.currentOutput?.device_id,
+                            ),
+                          );
+
+                        self.connect("notify::selected", () => {
+                          const idx = self.get_selected();
+                          const output = outputs[idx];
+
+                          const speaker = wp.audio.speakers.find(
+                            (s) => s.device_id === output.id,
+                          );
+
+                          if (speaker) {
+                            speaker.set_is_default(true);
+                            setState({
+                              ...state.get(),
+                              currentOutput: speaker,
+                            });
+                          }
+                        });
+                      }}
+                    />
+                  </box>
+
+                  <box orientation={Gtk.Orientation.HORIZONTAL} spacing={8}>
+                    <image
+                      iconName="audio-input-microphone"
+                      iconSize={Gtk.IconSize.NORMAL}
+                    />
+                    <Gtk.DropDown
+                      hexpand={true}
+                      class="dropdown"
+                      $={(self) => {
+                        const inputs = s.allDevices.filter(
+                          (s) => s.inputRoutes.length !== 0,
+                        );
+
+                        const store = new Gtk.StringList({
+                          strings: inputs.map((d) => d.description),
+                        });
+                        self.set_model(store);
+
+                        if (s.currentOutput)
+                          self.set_selected(
+                            inputs.findIndex(
+                              (d) => d.id === s.currentInput?.device_id,
+                            ),
+                          );
+
+                        self.connect("notify::selected", () => {
+                          const idx = self.get_selected();
+                          const input = inputs[idx];
+
+                          const microphone = wp.audio.microphones.find(
+                            (s) => s.device_id === input.id,
+                          );
+
+                          if (microphone) {
+                            microphone.set_is_default(true);
+                            setState({
+                              ...state.get(),
+                              currentInput: microphone,
+                            });
+                          }
+                        });
+                      }}
+                    />
+                  </box>
+                </box>
+              )}
+            </With>
+          </box>
+          <Gtk.Separator />
+
+          <box orientation={Gtk.Orientation.HORIZONTAL}>
+            <image iconName="audio-headphones" iconSize={Gtk.IconSize.NORMAL} />
+            <slider
+              min={0}
+              max={1}
+              hexpand={true}
+              value={createBinding(wp.defaultSpeaker, "volume")}
+              onChangeValue={({ value }) => wp.defaultSpeaker.set_volume(value)}
+            />
+          </box>
+
+          <box orientation={Gtk.Orientation.HORIZONTAL}>
+            <image
+              iconName="audio-input-microphone"
+              iconSize={Gtk.IconSize.NORMAL}
+            />
+            <slider
+              min={0}
+              max={1.5}
+              hexpand={true}
+              value={createBinding(wp.defaultMicrophone, "volume")}
+              onChangeValue={({ value }) =>
+                wp.defaultMicrophone.set_volume(value)
+              }
+            />
+          </box>
+        </box>
+      </popover>
     </box>
   );
 }
