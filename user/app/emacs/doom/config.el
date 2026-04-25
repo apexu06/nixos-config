5386b3d1697755f0a70f995c8193103d5fa6c209
apexu <jj.zelger@proton.me>
2026-04-25 17:40:51 +0200

    add vterm

--- user/app/emacs/doom/config.el
+++ user/app/emacs/doom/config.el
@@ -16,17 +16,24 @@
 (after! lsp-mode
   (require 'lsp-ui-doc))
 
-(after! lsp-ui
+(use-package! lsp-ui
+  :config
   (setq
    lsp-ui-sideline-enable nil
    lsp-ui-doc-position 'at-point
    lsp-ui-doc-show-with-cursor nil))
 
+(after! vterm
+  (setq vterm-shell (executable-find "fish"))
+  (evil-define-key 'normal vterm-mode-map (kbd "C-h") #'evil-window-left)
+  (evil-define-key 'normal vterm-mode-map (kbd "C-j") #'evil-window-down)
+  (evil-define-key 'normal vterm-mode-map (kbd "C-k") #'evil-window-up)
+  (evil-define-key 'normal vterm-mode-map (kbd "C-l") #'evil-window-right))
 
 (map! :leader
       :n "`" #'evil-switch-to-windows-last-buffer
       :g "DEL" #'kill-buffer-and-window
-      :n "o" #'dired-jump
+      :desc "Open dired" :n "o" #'dired-jump
 
       :n "s f" #'projectile-find-file
       :n "s F" #'find-file
@@ -45,19 +52,31 @@
 
       :n "l g" #'magit
 
+      :n "w v" #'evil-window-split
+      :n "w h" #'evil-window-vsplit
+
+      :desc "terminal" "t" nil
+      :desc "Toggle terminal" "t t" #'+vterm/toggle
+      :desc "Fullscreen terminal" "t w" #'+vterm/here
+
       :n "x" #'flycheck-list-errors
       :n "] e" #'flycheck-next-error
       :n "[ e" #'flycheck-previous-e)
 
+
 (map!
  :n "L" #'next-buffer
  :n "H" #'previous-buffer
- :g "C-h" #'evil-window-left
- :g "C-j" #'evil-window-down
- :g "C-k" #'evil-window-up
- :g "C-l" #'evil-window-right)
+ "C-h" #'evil-window-left
+ "C-j" #'evil-window-down
+ "C-k" #'evil-window-up
+ "C-l" #'evil-window-right
+ "M-h" #'evil-window-decrease-width
+ "M-l" #'evil-window-increase-width
+ "M-k" #'evil-window-increase-height
+ "M-j" #'evil-window-decrease-height
 
-(map!
  :n "K" #'lsp-ui-doc-glance
  :leader "RET" #'+workspace:new
- :leader "d" #'+workspace:delete)
+ :leader "d" #'+workspace:delete
+ )
